653b2708f371e6b2f80589bec3ab557343cb83b5
Carlos Cordero <carlos.cordero@odigeo.com>
2017-03-17 16:04:48 +0100

    Added listeners

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -8,8 +8,6 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
-        setTitle("Numbers");
-    }
 
+    }
 }
